56347029e3b8908e886353892cd31c8de9e686a3
takumitakeno <matchball.shine@gmail.com>
2020-05-08 02:05:58 +0000

    [add]newsコントローラ機能追加

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,10 +4,30 @@ class NewsController < ApplicationController
   end
 
   def show
+    @news = News.find(params[:id])
   end
   
+  def create
+    news = News.new(news_params)
+    news.create
+    redirect_to new_news_path
+  end
+
+  def edit
+    @news = News.find(prams[:id])
+    @news.update
+    redirect_to new_news_path
+  end
+
+  def destroy
+    news = News.find(params[:id])
+    news.destroy
+    redirect_to new_news_path
+  end
+
   private
-    def news_params
-      params.require(:news).permit(:title, :content)
-    end
+  def news_params
+    params.require(:news).permit(:title, :content)
+  end
+  
 end
