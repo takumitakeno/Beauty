5f72c8f412cb843d96f87a8f86de09251bcdc9d7
takumitakeno <matchball.shine@gmail.com>
2020-05-09 06:35:56 +0000

    [fix]ログアウトエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def usersign_in_path_for(resource)
     managements_path
   end
-  def after_sign_out_path_for(resource)
+  def user_sign_out_path_for(resource)
     root_path
   end
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 # Rails.application.routes.draw do
-  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 #   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # end
 Rails.application.routes.draw do
-  root 'homes#top'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :admins,
+  devise_for :users,
     controllers: {
-      sessions:'admins/sessions',
-      registrations: 'admins/registrations'
-  }
+      registrations: 'users/registrations',
+      sessions: 'users/sessions'   
+    } 
+  root 'homes#top'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   
   get 'news/new' => 'news#new'
   resources :managements
   resources :news
-  devise_scope :admin do
-    get '/admins/sign_out' => 'devise/sessions#destroy'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  
 end
