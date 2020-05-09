55c614d618dcf0c5307b4b78d97978aeef85ab46
takumitakeno <matchball.shine@gmail.com>
2020-05-09 06:57:30 +0000

    [add]管理画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # Rails.application.routes.draw do
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 #   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # end
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users,
     controllers: {
       registrations: 'users/registrations',
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_061734) do
+ActiveRecord::Schema.define(version: 2020_05_09_064555) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_05_09_061734) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
